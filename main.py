5c7fed47d6f667a7db3d48f8f873aa02261fe01c
WCbeardown <cinderella_321@knh.biglobe.ne.jp>
2022-02-21 08:50:32 +0900

    Initial Commit

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-st.title('羽曳野レイティング　比較グラフ')
+st.write('使い方：上の「＞」を押して、会員番号と年を入力')
+
+st.write('レイティング　比較グラフ')
+st.write('羽曳野・若葉・奈良・HPC (2022.2.20現在)')
 
 rating_data = pd.read_csv("rating_data_all.csv", index_col=0)
 #会員番号入力
 kaiin = [1,2,3,4,5]
-kaiin[0] = st.sidebar.number_input("1人目の会員番号",90000,2500000,1800280)
-kaiin[1] = st.sidebar.number_input("2人目の会員番号",90000,2500000,1900131)
-kaiin[2] = st.sidebar.number_input("3人目の会員番号",90000,2500000,1800280)
-kaiin[3] = st.sidebar.number_input("4人目の会員番号",90000,2500000,1800280)
-kaiin[4] = st.sidebar.number_input("5人目の会員番号",90000,2500000,1800280)
+kaiin[0] = st.sidebar.number_input("1人目の会員番号",90000,2500000,1802222)
+kaiin[1] = st.sidebar.number_input("2人目の会員番号",90000,2500000,1802222)
+kaiin[2] = st.sidebar.number_input("3人目の会員番号",90000,2500000,1802222)
+kaiin[3] = st.sidebar.number_input("4人目の会員番号",90000,2500000,1802222)
+kaiin[4] = st.sidebar.number_input("5人目の会員番号",90000,2500000,1802222)
 
 #年間まとめの計算開始と終了年
 year_s = st.sidebar.number_input("開始年",2000,2030,2018)
@@ -63,7 +66,7 @@ st.pyplot(fig)
 
 
 #年間まとめの表
-st.title('レイティング　年平均比較表')
+st.write('レイティング　年平均比較表')
 #hajime=[]
 #owari=[]
 #for j in range(5):
@@ -85,6 +88,20 @@ for j in range(5):
 nen_heikin_matome = pd.DataFrame(temp,columns = matome)
 st.dataframe(nen_heikin_matome)
 
-st.title('1人目の詳細データ')
+#個人データの表示
 rating_data=rating_data.set_index('場所')
+
+st.write('1人目の詳細データ')
 st.table(rating_data[rating_data["会員番号"] == kaiin[0]])
+
+st.write('2人目の詳細データ')
+st.table(rating_data[rating_data["会員番号"] == kaiin[1]])
+
+st.write('3人目の詳細データ')
+st.table(rating_data[rating_data["会員番号"] == kaiin[2]])
+
+st.write('4人目の詳細データ')
+st.table(rating_data[rating_data["会員番号"] == kaiin[3]])
+
+st.write('5人目の詳細データ')
+st.table(rating_data[rating_data["会員番号"] == kaiin[4]])
